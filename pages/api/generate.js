6e1917fd8c808609402e742f939e832fb6129652
Dev EvertonJackson <dev.evertonjackson@gmail.com>
2023-07-15 15:07:18 -0300

    fist commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -29,10 +29,13 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
-      temperature: 0.6,
+      temperature: 0.9,
+      max_tokens: 500,
     });
+
+    console.log(completion.data.choices[0].text)
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -51,12 +54,21 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return ` Orientação para registro de imoveis do cartorio de goias.
+           Só responda as questões que envolva legislação de goiás.
+           Caso seja informado uma pergunta com um tema diferente, responda não tenho essas informações.
+           Sempre gerar texto resumido com 50 caracteris.
+           Sempre no final do texto coloque o Atenciosamente Cartório.
+           Abaixo são as perguntas e respostas para responder os clientes do cartório.
+            Pergunta Cliente: Quais os tipos de atos se pratica em um registro de imovel.
+            Reposta Cartorio: compra e venda ,alienação fiduciaria, casamento.
+            O casamento estabelece a comunhão plena de vida, com base na igualdade de direitos e deveres dos cônjuges. Pelos princípios da continuidade registral e da especialidade subjetiva, todas as alterações de estado civil deverão ser averbadas na matrícula.
+            Previsão Legal – artigos 1.511 e ss. e 1.653 e ss. do Código Civil; artigos 167, 176, 217, 225, 244, 246 e ss. da Lei n. 6.015/1973.
+            Documentos necessários:
+            Requerimento do interessado, com firma reconhecida, indicando expressamente o imóvel e seu número de matrícula, bem como a qualificação do cônjuge (nome, nacionalidade, profissão, número do RG, CPF e domicílio), acompanhado da cópia autenticada do RG e CPF do mesmo;
+            Certidão de casamento, na forma original ou em cópia autenticada;
+            Se o regime de bens do casamento não for o da comunhão parcial, ou o obrigatoriamente estabelecido, apresentar certidão de registro do pacto antenupcial emitida pelo Registro de Imóveis em que foi registrado ou o próprio pacto antenupcial, em sua via original ou em cópia autenticada, desde que, neste caso, conste carimbo ou etiqueta do Registro de Imóveis declarando, resumidamente, os atos praticados*.
+            O pacto antenupcial deve ser registrado no domicílio do casal. Caso o domicílio seja nesta Circunscrição, deverá apresentar via original do pacto antenupcial para registro no livro 03 desta Serventia.
+            Pergunta Cliente: ${capitalizedAnimal}
+            Reposta Cartorio:`;
 }
